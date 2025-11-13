4b2f86c9671c9faaee9c36cecddfde8c4a6f7280
arsyangela <54343318+arsyangela@users.noreply.github.com>
2025-11-13 13:03:18 +0800

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.DataFrame('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+weight= df['weight']/(df['height'])^2
+if weight > 25:
+    weightf = 1
+else: 
+    weight = 0
+
+df['overweight'] = weightf
+
 
 # 3
 
 
-# 4
-def draw_cat_plot():
-    # 5
-    df_cat = None
+# # 4
+# def draw_cat_plot():
+#     # 5
+#     df_cat = None
 
 
-    # 6
-    df_cat = None
+#     # 6
+#     df_cat = None
     
 
-    # 7
+#     # 7
 
 
 
-    # 8
-    fig = None
+#     # 8
+#     fig = None
 
 
-    # 9
-    fig.savefig('catplot.png')
-    return fig
+#     # 9
+#     fig.savefig('catplot.png')
+#     return fig
 
 
-# 10
-def draw_heat_map():
-    # 11
-    df_heat = None
+# # 10
+# def draw_heat_map():
+#     # 11
+#     df_heat = None
 
-    # 12
-    corr = None
+#     # 12
+#     corr = None
 
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
+#     # 13
+#     mask = None
 
 
 
-    # 16
+#     # 14
+#     fig, ax = None
+
+#     # 15
+
+
+
+#     # 16
     fig.savefig('heatmap.png')
     return fig
